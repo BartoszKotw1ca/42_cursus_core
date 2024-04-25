a07e6c0a88201a88810de3550762489e711efe6a
Bartoromeo1234 <bartekbkotwica@gmail.com>
2024-04-25 10:15:04 +0200

    pipex

---
+++ pipex/pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkotwica <bkotwica@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 18:41:16 by bkotwica          #+#    #+#             */
+/*   Updated: 2024/04/23 12:43:31 by bkotwica         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "mylibft/mylibft.h"
+# include <stdio.h> // perror, 
+# include <string.h> // strerror, 
+# include <stdlib.h> // malloc, exit, 
+# include <sys/wait.h> //
+# include <unistd.h> // read, write,
+// access, dup, dup2, execve, pipe
+# include <fcntl.h> // open, close
+# include <sys/types.h> // fork, wait
+# include <sys/wait.h> // wait, waitpid
+
+#endif
