45d94cbe7f9601e165d5b43e6811b0b724f63983
Bartosz Kotwica <bartekbkotwica@gmail.com>
2024-06-27 15:30:16 +0200

    better for one philo

--- Philosophers/philo/for_one.c
+++ Philosophers/philo/for_one.c
@@ -6,7 +6,7 @@
 /*   By: bkotwica <bkotwica@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 15:06:39 by bkotwica          #+#    #+#             */
-/*   Updated: 2024/06/27 15:25:16 by bkotwica         ###   ########.fr       */
+/*   Updated: 2024/06/27 15:30:06 by bkotwica         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	for_one_phil(t_node *node)
 {
-	printf("0 1 has taken a fork\n");
+	print_status(node, 1, "has taken a fork", YELLOW);
 	usleep(node->time_to_die * 1000);
-	printf("%lld 1 died\n", get_time() - node->start_time);
+	print_status(node, 1, "died", RED);
 	free_data(node);
 	free(node->last_food);
 	free(node->dead);
