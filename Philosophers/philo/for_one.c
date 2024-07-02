e21a62a6cec1e0ca93d43b14799f911237067f35
Bartosz Kotwica <bartekbkotwica@gmail.com>
2024-07-02 19:21:17 +0200

    no problem with one phil

--- Philosophers/philo/for_one.c
+++ Philosophers/philo/for_one.c
@@ -6,7 +6,7 @@
 /*   By: bkotwica <bkotwica@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 15:06:39 by bkotwica          #+#    #+#             */
-/*   Updated: 2024/06/28 13:07:31 by bkotwica         ###   ########.fr       */
+/*   Updated: 2024/07/02 19:20:34 by bkotwica         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	for_one_phil(t_node *node)
 {
+	node->id = 1;
 	print_status(node, 1, "has taken a fork", YELLOW);
 	usleep(node->time_to_die * 1000);
 	print_status(node, 1, "died", RED);
