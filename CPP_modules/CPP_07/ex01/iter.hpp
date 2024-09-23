68157d2301657b2275d94dca25d93096ccc5a675
Bartoromeo1234 <bartekbkotwica@gmail.com>
2024-09-23 20:59:58 +0200

    CPP_07->ex01 done

--- CPP_modules/CPP_07/ex01/iter.hpp
+++ CPP_modules/CPP_07/ex01/iter.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <iostream>
+
 template <typename T>
-void iter(T &array, int len, void (*fun)(T &var))
+void iter(T *array, int len, void (*fun)(T &var))
 {
     for (int i = 0; i < len; i++)
         fun(array[i]);
--- CPP_modules/CPP_07/ex01/main.cpp
+++ CPP_modules/CPP_07/ex01/main.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
 #include "iter.hpp"
+
+void print(int &x)
+{
+    std::cout << x << std::endl;
+}
 
 void add_one(int &x)
 {
     x += 1;
-}
-
-void print(int x)
-{
-    std::cout << x << std::endl;
 }
 
 int main(void)
@@ -16,12 +15,13 @@ int main(void)
     int array[] = {1, 2, 3, 4, 5};
     
     std::cout << "\nBefore adding one to numbers!" << std::endl;
-    // iter(array, 5, print);
-    
+    iter(array, 5, print);
+
     iter(array, 5, add_one);
     
     std::cout << "\nADDED:" << std::endl;
     
-    // iter(array, 5, print);
+    iter(array, 5, print);
+
     return 0;
 }
